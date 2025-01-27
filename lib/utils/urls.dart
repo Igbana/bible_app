3229dc89335093b669acee0b3a794b79d31f4531
Igbana Israel <igbanaaondosoo13@gmail.com>
2025-01-27 21:24:43 +0100

    refactor: Created a url file for all urls

---
+++ lib/utils/urls.dart
@@ -0,0 +1,6 @@
+Uri notesUrl = Uri.parse('https://cpai.guidetryb.com/api-secured/users/notes');
+Uri videosUrl = Uri.parse('https://cpai.guidetryb.com/api/videos');
+Uri devUrl = Uri.parse('https://cpai.guidetryb.com/api/devotionals?month=');
+Uri studyplansUrl = Uri.parse('https://cpai.guidetryb.com/api/study-plans');
+Uri studyplanUrl = Uri.parse('https://cpai.guidetryb.com/api/study-plans/');
+Uri noteUrl = Uri.parse('https://cpai.guidetryb.com/api-secured/users/notes/');
